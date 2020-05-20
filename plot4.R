5df934bb4048d67afcb89843690c9dc48eef5faf
Arnold Chang <arnoldchang2013@gmail.com>
2020-05-19 23:33:00 -1000

    upload plots

---
+++ plot4.R
@@ -0,0 +1,15 @@
+filename = "/Users/arnoldchang/Downloads/household_power_consumption.txt"
+colClasses <- c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric","numeric","numeric")
+df1 <- read.table(filename,header=TRUE,sep=";", na.strings = "?", colClasses = colClasses)
+
+df1$Date <- as.Date(df1$Date, "%d/%m/%Y")
+
+
+df1 <- subset(df1, Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+df1 <- df1[complete.cases(df1),]
+
+dateTime <- paste(df1$Date, df1$Time)
+df1 <- cbind(dateTime,df1)
+df1$dateTime <- as.POSIXct(dateTime)
+
+df1 <- subset(df1, select = -c(Date,Time))
